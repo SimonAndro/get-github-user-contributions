3f31d4826182b2ab0d5de874e86c2ae26654d5a1
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-09-09 01:52:21 +0800

    using curl instead of file_get_contents

--- index.php
+++ index.php
@@ -6,19 +6,28 @@ require 'phpQuery/phpQuery.php'; //require php query
  * get github user contributions, avatar and nickname
  */
 
- // github user names
+// github user names
 $userNames = [
     "simonandro",
     "admin368",
-  ];
-  
-  foreach ($userNames as $u) {
-    $markup = file_get_contents("https://github.com/" . $u); // fetch user github page
-    $doc = phpQuery::newDocumentHTML($markup);  // create php query document
-  
+];
+
+foreach ($userNames as $u) {
+    //$markup = file_get_contents("https://github.com/" . $u); // fetch user github page
+
+    $url = "https://github.com/" . $u;
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $markup = curl_exec($curl);
+    curl_close($curl);
+
+    $doc = phpQuery::newDocumentHTML($markup); // create php query document
+
     $avatar = pq(".avatar-user")->slice(-1)->attr("src"); // get github user avatar
     $nickname = trim(pq(".p-nickname")->html()); // get github username
-  
+
     $contributions = pq(".js-calendar-graph-svg g g")->slice(-1)->find("rect"); //get contributions in current week
     $contributions_this_week = array("0" => 0, "1" => 0, "2" => 0, "3" => 0, "4" => 0, "5" => 0, "6" => 0); //["S","M","T","W","T","F","S"];
     $count = 0;
@@ -26,10 +35,10 @@ $userNames = [
     foreach ($contributions as $c) {
         $contributions_this_week[$count++] = pq($c)->attr("data-count");
     }
-  
+
     $gitHubUser["avatar"] = $avatar;
     $gitHubUser["nickname"] = $nickname;
     $gitHubUser["contributions"] = $contributions_this_week;
-  
+
     print_r($gitHubUser); // github user data
-  }
+}
