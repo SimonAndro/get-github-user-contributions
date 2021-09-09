6dcb0c954838c433682d8911c48cf61182b77f33
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-09-09 13:51:07 +0800

    user curl to make parallel requests at once

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require 'phpQuery/phpQuery.php'; //require php query
 
+set_time_limit(0);
+
 /**
  * get github user contributions, avatar and nickname
  */
@@ -12,18 +14,19 @@ $userNames = [
     "admin368",
 ];
 
-foreach ($userNames as $u) {
-    $markup = file_get_contents("https://github.com/" . $u); // fetch user github page
+//create an array of gthub user home page urls
+$url = "https://github.com/";
+$urls = array();
+foreach( $userNames as $u)
+{
+    array_push($urls,$url.$u);
+}
 
-    // $url = "https://github.com/" . $u;
+$res = multiple_threads_request($urls); // fetch user github  users home pages
 
-    // $curl = curl_init($url);
-    // curl_setopt($curl, CURLOPT_URL, $url);
-    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1000); 
-    // curl_setopt($curl, CURLOPT_TIMEOUT, 400); //timeout in seconds
-    // $markup = curl_exec($curl);
-    // curl_close($curl);
+foreach ($urls as $url) {
+
+    $markup = $res[$url];  // get user homepage markup
 
     $doc = phpQuery::newDocumentHTML($markup); // create php query document
 
@@ -43,4 +46,43 @@ foreach ($userNames as $u) {
     $gitHubUser["contributions"] = $contributions_this_week;
 
     print_r($gitHubUser); // github user data
+}
+
+
+/**
+ * Makes parallel curl requests at once
+ */
+function multiple_threads_request($nodes)
+{
+    set_time_limit(0);
+
+    $mh = curl_multi_init();
+    $curl_array = array();
+    foreach ($nodes as $i => $url) {
+        $curl_array[$i] = curl_init($url);
+        curl_setopt($curl_array[$i], CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl_array[$i], CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl_array[$i], CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl_array[$i], CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl_array[$i], CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl_array[$i], CURLOPT_COOKIEFILE, "/tmp/cookie.txt");
+        curl_setopt($curl_array[$i], CURLOPT_COOKIEJAR, "/tmp/cookie.txt");
+        curl_multi_add_handle($mh, $curl_array[$i]);
+    }
+    $running = null;
+    do {
+        usleep(10000);
+        curl_multi_exec($mh, $running);
+    } while ($running > 0);
+
+    $res = array();
+    foreach ($nodes as $i => $url) {
+        $res[$url] = curl_multi_getcontent($curl_array[$i]);
+    }
+
+    foreach ($nodes as $i => $url) {
+        curl_multi_remove_handle($mh, $curl_array[$i]);
+    }
+    curl_multi_close($mh);
+    return $res;
 }
